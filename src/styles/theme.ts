11fd8c5ba711f894e6262be5e73e0ea80218d2df
sopoour <sophia.auer@googlemail.com>
2025-08-20 15:35:19 +0200

    Farben entwurf 2

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -5,17 +5,17 @@ const theme = {
   colors: {
     bg: {
       default: '#E0F5F6',
-      contrast: '#347D80',
-      defaultBlur: 'rgba(52,125, 128, 0.5)'
+      contrast: '#0C3037',
+      defaultBlur: 'rgba(52,125, 128, 0.8)'
     },
     fg: {
-      default: '#347D80',
+      default: '#0C3037',
       contrast: '#E0F5F6',
       inactive: 'rgba(255, 255, 255, 0.75)',
     },
     accent: {
-      flamingo: '#FF7875',
-      yellow: '#FFF747',
+      flamingo: '#7F7364',
+      yellow: '#FFA361',
     },
   },
   filters: {
